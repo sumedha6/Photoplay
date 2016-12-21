0f2cde3fe4f776c6c9ef1b57069ee05c07cea44f
himanshu-kushwaha <himanshukushwaha@outlook.com>
2016-12-21 17:50:58 +0530

    updated app.js

--- app.js
+++ app.js
@@ -21,17 +21,16 @@ app.post('/upload', function(req, res) {
         image: req.body.data,
         // id: uuid.v4()
     };
-
     rooms.push(room);
 
     res.redirect("/");
 });
-// app.get('/admin/rooms', function (req, res) {
-//     res.render("rooms", {
-//         title: "Admin Rooms",
-//         rooms: rooms
-//     });
-// });
+app.get('/admin/rooms', function(req, res) {
+    res.render("rooms", {
+        title: "Admin Rooms",
+        rooms: rooms
+    });
+});
 
 // app.get('/admin/rooms/add', function (req, res) {
 //     res.render("add");
