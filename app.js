4d407df2af5ad0271057838df9b427933880a4eb
himanshu-kushwaha <himanshukushwaha@outlook.com>
2016-12-21 14:20:09 +0530

    updated index.jade

--- app.js
+++ app.js
@@ -12,66 +12,66 @@ app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-    res.render("index", { title: "Home"});
+app.get('/', function(req, res) {
+    res.render("index", { title: "Home" });
 });
 
-app.get('/admin/rooms', function (req, res) {
-    res.render("rooms", {
-        title: "Admin Rooms",
-        rooms: rooms
-    });
-});
+// app.get('/admin/rooms', function (req, res) {
+//     res.render("rooms", {
+//         title: "Admin Rooms",
+//         rooms: rooms
+//     });
+// });
 
-app.get('/admin/rooms/add', function (req, res) {
-    res.render("add");
-});
+// app.get('/admin/rooms/add', function (req, res) {
+//     res.render("add");
+// });
 
-app.post('/admin/rooms/add', function (req, res) {
-    var room = {
-        name: req.body.name,
-        id: uuid.v4()
-    };
+// app.post('/admin/rooms/add', function (req, res) {
+//     var room = {
+//         name: req.body.name,
+//         id: uuid.v4()
+//     };
 
-    rooms.push(room);
+//     rooms.push(room);
 
-    res.redirect("/admin/rooms");
-});
+//     res.redirect("/admin/rooms");
+// });
 
-app.get('/admin/rooms/edit/:id', function(req, res){
-    var roomId = req.params.id;
+// app.get('/admin/rooms/edit/:id', function(req, res){
+//     var roomId = req.params.id;
 
-    var room = _.find(rooms, r => r.id === roomId);
-    if(!room){
-        res.sendStatus(404);
-        return;
-    }
+//     var room = _.find(rooms, r => r.id === roomId);
+//     if(!room){
+//         res.sendStatus(404);
+//         return;
+//     }
 
-    res.render("edit", { room });
-});
+//     res.render("edit", { room });
+// });
 
-app.post('/admin/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
+// app.post('/admin/rooms/edit/:id', function (req, res) {
+//     var roomId = req.params.id;
 
-    var room = _.find(rooms, r => r.id === roomId);
-    if(!room){
-        res.sendStatus(404);
-        return;
-    }
+//     var room = _.find(rooms, r => r.id === roomId);
+//     if(!room){
+//         res.sendStatus(404);
+//         return;
+//     }
 
-    room.name = req.body.name;
+//     room.name = req.body.name;
 
-    res.redirect("/admin/rooms");
-});
+//     res.redirect("/admin/rooms");
+// });
 
-app.get('/admin/rooms/delete/:id', function(req, res){
-    var roomId = req.params.id;
+// app.get('/admin/rooms/delete/:id', function(req, res){
+//     var roomId = req.params.id;
 
-    rooms = rooms.filter(r => r.id !== roomId);
+//     rooms = rooms.filter(r => r.id !== roomId);
 
-    res.redirect("/admin/rooms");
-});
+//     res.redirect("/admin/rooms");
+// });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log('Chat app listening on port 3000!');
 });
