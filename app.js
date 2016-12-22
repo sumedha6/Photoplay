185d0b983dacce4edf5f50cfd3005343e78dfed7
Sumedha Khurana <Sumedha Khurana>
2016-12-22 17:29:26 +0530

    app.js updated

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ var bodyParser = require("body-parser");
 var uuid = require("node-uuid");
 var _ = require("lodash");
 
-<<<<<<< HEAD
-app.set("views", "./public");
-app.set('view engine', 'html');
-app.set('view engine', 'ejs');
-//app.use(express.static("public"));
-
-=======
->>>>>>> befcfc5b1dde121b0757d291c6fe3296433678b2
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,19 +14,6 @@ app.get('/', function(req, res) {
 });
 
 app.post('/upload', function(req, res) {
-<<<<<<< HEAD
-    var room = {
-        name: req.body.data,
-        id: uuid.v4()
-    };
-    rooms.push(room);
-
-    res.redirect('/');
-    res.render("upload", { title: "upload" });
-    res.render('search.ejs', output);
-});
-=======
->>>>>>> befcfc5b1dde121b0757d291c6fe3296433678b2
 
     res.redirect(__dirname + '/public/upload.html');
 });
@@ -43,10 +22,5 @@ app.post('/upload', function(req, res) {
 //     res.sendFile(__dirname + '/public/upload.html');
 // });
 app.listen(3000, function() {
-<<<<<<< HEAD
-    console.log('Chat app listening on port 3000!');
-});
-=======
     console.log('listening on port 3000!');
 });
->>>>>>> befcfc5b1dde121b0757d291c6fe3296433678b2
