21e71aec560c485e94acb0e90e5b96aac6095dd8
sumedha6 <sumedha.khurana96@gmail.com>
2016-12-25 17:26:50 +0530

    mongo db created

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var bodyparser = require('body-parser');
 var uuid = require('node-uuid');
 var emotions = './data/emotions.json';
 var jsonfile = require('jsonfile');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 
+var url = 'mongodb://localhost:27017/photoplay';
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyparser.json({ strict: false }));
@@ -45,18 +48,20 @@ app.post('/', function(req, res) {
     form.parse(req);
 
 });
-app.post('/upload', function(req, res) {
-    console.log("req", req.body);
-
-    var emotion = {
-        emotions: req.body,
-        id: uuid.v4(),
-    };
-    jsonfile.writeFile(emotions, emotion, function(err) {
-        console.error(err)
-    });
-    res.json(emotion);
-});
+// app.post('/upload', function(req, res) {
+//     console.log("req", req.body);
+//            var emotion = {
+//             emotions: req.body,
+//             id: uuid.v4(),
+//         };
+//
+//     jsonfile.writeFileSync(emotions, emotion, [fs.writeFileSync]); //, function(err) {
+//     //console.error(err)
+//     //});
+//     res.json(emotion);
+//     res.mongoDB(emotion);
+//
+// });
 
 app.get('/upload', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/upload.html'));
@@ -65,4 +70,40 @@ app.get('/upload', function(req, res) {
 
 var server = app.listen(3000, function() {
     console.log('Server listening on port 3000');
+});
+
+app.post('/upload', function(req, res) {
+    console.log("req", req.body);
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            //HURRAY!! We are connected.
+            console.log('Connection established to', url);
+
+            // do some work here with the database.
+
+
+            var emotion = {
+                emotions: req.body,
+                id: uuid.v4(),
+            };
+
+            var collection = db.collection('scores');
+            collection.insert(emotion, function(err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+                }
+            });
+
+
+            // jsonfile.writeFileSync(emotions, emotion, [fs.writeFileSync]); //, function(err) {
+            //console.error(err)
+            //});
+            res.json(emotion);
+            db.close();
+        }
+    });
 });
