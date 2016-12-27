0c0083936521027f29b63fa5e1ef7fd85a5a368e
sumedha6 <sumedha.khurana96@gmail.com>
2016-12-27 14:35:52 +0530

    make it better

--- public/js/emotionapi.js
+++ public/js/emotionapi.js
@@ -1,88 +1,94 @@
- $('#btn').click(function() {
-            var file = document.getElementById('upload-input').files[0];
-            var apiUrl = "https://api.projectoxford.ai/emotion/v1.0/recognize";
-            var apiKey = "6e1e8ccb31204ae7a636b502b3c93511";
-            var result = $('#response');
-            var value = response.faceRectangle;
-
-            CallAPI(file, apiUrl, apiKey);
-            // Emotion(value);
-            ProcessResult(file);
-
-        });
+$('#btn').click(function() {
+    var file = document.getElementById('upload-input').files[0];
+    var apiUrl = "https://api.projectoxford.ai/emotion/v1.0/recognize";
+    var apiKey = "6e1e8ccb31204ae7a636b502b3c93511";
+    var result = $('#response');
+    var value = response.faceRectangle;
+    console.log("filename:::::", file.name);
 
 
-        function CallAPI(file, apiUrl, apiKey) {
+    CallAPI(file, apiUrl, apiKey);
+    // Emotion(value);
+    ProcessResult(file);
+
+});
 
 
-            $.ajax({
-                    url: apiUrl,
-                    beforeSend: function(xhrObj) {
-                        // Request headers
-                        xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
-                        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
-                    },
-                    type: "POST",
-                    processData: false,
-                    data: file,
-                    dataType: 'json'
+function CallAPI(file, apiUrl, apiKey) {
 
-                })
-                .done(function(response) {
-                    console.log(".done", response)
-                    for (var i = 0; i < response.length; i++) {
-                        var data = JSON.stringify(response[i].scores);
+    var name = file.name;
+    $.ajax({
+            url: apiUrl,
+            beforeSend: function(xhrObj) {
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+            },
+            type: "POST",
+            processData: false,
+            data: file,
+            dataType: 'json'
 
-
-                        $('#response').append(data);
-
-                        console.log("MY ARRAY", data)
-                        $.ajax({
-                            url: '/upload',
-                            type: 'POST',
-                            data: data,
-                            // processData: false,
-                            contentType: 'application/json',
-                            success: function(data) {
-                                console.log('upload successful', data);
-                            },
-
-                        });
-
-                    }
+        })
+        .done(function(response) {
+            console.log(".done", response)
+            ProcessResult(response, name);
+            //  for (var i = 0; i < response.length; i++) {
+            //      var data = JSON.stringify(response[i].scores);
 
 
+            //      $('#response').append(data);
+
+            //      console.log("MY ARRAY", data)
+            //      $.ajax({
+            //          url: '/upload',
+            //          type: 'POST',
+            //          data: { emotion: data, name: name },
+            //          // processData: false,
+            //          contentType: 'application/json',
+            //          success: function(data) {
+            //              console.log('upload successful', data);
+            //          },
+
+            //      });
+
+            //  }
 
 
-                })
 
 
-            .fail(function(error) {
-                $("#response").text(error.getAllResponseHeaders());
-            });
-        }
+        })
 
 
-        function ProcessResult(response) {
-            for (var i = 0; i < response.length; i++) {
-                var data = JSON.stringify(response[i].scores);
+    .fail(function(error) {
+        $("#response").text(error.getAllResponseHeaders());
+    });
+}
 
 
-                $('#response').append(data);
+function ProcessResult(response, name) {
+    for (var i = 0; i < response.length; i++) {
+        var data = JSON.stringify(response[i].scores);
 
-            }
-            console.log(":data::", data);
-            $.ajax({
-                url: '/upload',
-                type: 'POST',
-                data: data,
-                // processData: false,
-                contentType: 'application/json',
-                success: function(data) {
-                    console.log('upload successful', data);
-                },
 
-            });
-            console.log('data', data);
+        $('#response').append(data);
 
-        }
+    }
+    console.log(":data::", data);
+    $.ajax({
+        url: '/upload',
+        type: 'POST',
+        data: JSON.stringify({
+             emotion: data,
+             name: name
+         }),
+        // processData: false,
+        contentType: 'application/json',
+        success: function(data) {
+            console.log('upload successful', data);
+        },
+
+    });
+    console.log('data', data, name);
+
+}
