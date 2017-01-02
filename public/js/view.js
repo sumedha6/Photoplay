6339cd701832e250f40fa6edad200f9a92a02885
sumedha6 <sumedha.khurana96@gmail.com>
2017-01-02 22:13:33 +0530

    view

--- public/js/view.js
+++ public/js/view.js
@@ -1,21 +1,32 @@
+//<![CDATA[
 $('#btn2').click(function() {
-
-function handler() {
-    var result;
-
+    console.log("hel");
     $.ajax({
         url: '/view',
-           type: 'get',
-           dataType: 'jsonp',
-           jsonp: 'jsonp',
-           success: function (data) {
-               console.log('success', data);
-           },
-           error: function (XMLHttpRequest, textStatus, errorThrown) {
-               console.log('error', errorThrown);
-           }
-       });
+        type: 'get',
+        dataType: 'json',
+        jsonp: 'json', // mongod is expecting the parameter name to be called "jsonp"
+        contentType: "image/jpg",
 
-       return result;
-   }
+        success: function(data) {
+            console.log('success', data);
+            console.log('SUMEDHA');
+            var array = data;
+            console.log("my array", array)
+
+            for (var i = 0; i < array.length; i++) {
+                var element = array[i];
+                var $div = $("#img");
+                var src = "images/" + array[i];
+                console.log(src)
+                $("<img />").attr("src", src).appendTo($div);
+            }
+
+        },
+
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log('error', errorThrown);
+        }
+    });
+
 });
