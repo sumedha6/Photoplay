b1993ee6ce81a6c50752ecdeb7bdaa121c9add40
sumedha6 <sumedha.khurana96@gmail.com>
2017-01-08 13:12:09 +0530

    made it better

--- public/js/view.js
+++ public/js/view.js
@@ -23,10 +23,10 @@ $('#btn2').click(function() {
             for (var i = 0; i < array.length; i++) {
                 var element = array[i];
                 var $div = $("#img");
-                 src = "images/" + array[i];
-                 filename=array[i];
-                 srcc="quotes/"+array[i];
-                console.log('my files',filename)
+                src = "images/" + array[i];
+                filename = array[i];
+                srcc = "quotes/" + array[i];
+                console.log('my files', filename)
                 $("<img />").attr("src", src).appendTo($div);
                 Callfile();
             }
@@ -38,95 +38,116 @@ $('#btn2').click(function() {
         }
     });
 
-  function Callfile(){
-    console.log("Printing")
-      $.ajax({
-      url:srcc,
-      data:filename,
-      type:'POST',
-      datatype:'json',
+    function Callfile() {
+        console.log("Printing")
+        $.ajax({
+            url: srcc,
+            data: filename,
+            type: 'POST',
+            datatype: 'json',
 
 
-      success:function(data){
-        console.log('Sent data',data);
-      }
+            success: function(data) {
+                console.log('Sent data', data);
+            }
 
-    })
- }
+        })
+    }
 });
 
 var myData;
 var e;
-$('#searchme').click(function(){
-  var search= document.getElementById('search').value
+$('#searchme').click(function() {
+    var search = document.getElementById('search').value
 
-  console.log("Searching...",search);
+    console.log("Searching...", search);
 
 
 
-$.ajax({
-  url:'/search',
-  type:'POST',
-  data:search,
-  cache:false,
-  dataType:'text',
+    $.ajax({
+        url: '/search',
+        type: 'POST',
+        data: search,
+        cache: false,
+        dataType: 'text',
 
-  success:function (data){
-    console.log("Posted",data);
-   e=JSON.parse(data)
-    for(i=0;i<e.length;i++){
-      console.log(e[i]);
+        success: function(data) {
+            console.log("Posted", data);
+            e = JSON.parse(data)
+            for (i = 0; i < e.length; i++) {
+                console.log(e[i]);
 
-}
-filename(e);
-  }
-  })
-console.log("Searching...22");
-console.log("search",search);
+            }
+            var file = [];
+
+                   file[0] = e[0];
+                   var j = 0;
+                   for (var i = 0; i < e.length; i++) {
+                       for (var k = 0; k <= j; k++) {
+                           if (e[i] != file[j]) {
+                               j++;
+                               file[j] = e[i];
+                           }
+                       }
+                   }
+                   for (var i = 0; i < file.length; i++) {
+                                       var element = e[i];
+                                       var $div = $("#img2");
+                                       src = "images/" + file[i];
+                                       filename = file[i];
+                                       console.log('my files', filename)
+                                       $("<img />").attr("src", src).appendTo($div);
+            }
+
+            // filename(e);
+        }
+    })
+    console.log("Searching...22");
+    console.log("search", search);
 });
 
 
 
 
-function filename(e) {
-    $('#btn3').click(function() {
-      var file = [];
+// function filename(e) {
+//     $('#btn3').click(function() {
+//         var file = [];
 
-       file[0] = e[0];
-       var j = 0;
-       for (var i = 0; i < e.length; i++) {
-           for (var k = 0; k <= j; k++) {
-               if (e[i] != file[j]) {
-                   j++;
-                   file[j] = e[i];
-               }
-           }
-       }
-
-
-        $.ajax({
-            url: '/image',
-            method: 'GET',
-            jsonp: 'json',
-
-            contentType: "image/jpg",
-            success: function(data) {
-
-                console.log("Getting the image");
-                var stack = data;
-                console.log("my files", e)
+//         file[0] = e[0];
+//         var j = 0;
+//         for (var i = 0; i < e.length; i++) {
+//             for (var k = 0; k <= j; k++) {
+//                 if (e[i] != file[j]) {
+//                     j++;
+//                     file[j] = e[i];
+//                 }
+//             }
+//         }
 
 
-                for (var i = 0; i < file.length; i++) {
-                      var element = e[i];
-                      var $div = $("#img2");
-                      src = "images/" + file[i];
-                      filename = file[i];
-                      console.log('my files', filename)
-                      $("<img />").attr("src", src).appendTo($div);
-                  }
-              }
-          });
+//         $.ajax({
+//             url: '/image',
+//             method: 'GET',
+//             jsonp: 'json',
 
-      })
-  }
+//             contentType: "image/jpg",
+//             success: function(data) {
+
+//                 console.log("Getting the image");
+//                 var stack = data;
+//                 console.log("my files", e)
+
+
+//                 for (var i = 0; i < file.length; i++) {
+//                     var element = e[i];
+//                     var $div = $("#img2");
+//                     src = "images/" + file[i];
+//                     filename = file[i];
+//                     console.log('my files', filename)
+//                     $("<img />").attr("src", src).appendTo($div);
+//                 }
+//             }
+//         });
+
+//     })
+// }
